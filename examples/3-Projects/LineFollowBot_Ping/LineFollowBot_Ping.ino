2cf79b0a2923be472912f18b790e6011d0cd5c6a
Brett Menzies <scimor5@gmail.com>
2015-08-20 15:52:45 -0700

    Bring improvements to lineFollowBot_Line to other lineFollowBot examples

--- examples/3-Projects/LineFollowBot_Ping/LineFollowBot_Ping.ino
+++ examples/3-Projects/LineFollowBot_Ping/LineFollowBot_Ping.ino
@@ -8,40 +8,41 @@
 / Questions? Concerns? Bugs? email code@mymindsi.com
 /
 / This example expects 2 servos in pins 4 and 5
-/ and a ping sensor in pin 10
+/ and a ping sensor in pin 9
 /***************************************************/
 
-Servo leftServo, rightServo;
+Servo left, right;
 
-const int center = 93;
+const int center     =  90;
 const int hazardDist = 750;
 
 void setup() {
-  leftServo.attach(5);
-  rightServo.attach(4);
+  left.attach(5);
+  right.attach(4);
 
-  leftServo.write(0);
-  rightServo.write(180);
+  //driving straight
+  left.write(0);
+  right.write(180);
 }
 
 void loop() {
-  if (getPing(10) < hazardDist) {
+  if (getPing(9) < hazardDist) {
     //liberal coasting to prevent brownouts
-    leftServo.write(center);
-    rightServo.write(center);
+    left.write(center);
+    right.write(center);
     delay(500);
 
-    leftServo.write(180);
-    rightServo.write(center);
+    left.write(180);
+    right.write(center);
     delay(1000);
 
-    leftServo.write(center);
-    rightServo.write(center);
+    left.write(center);
+    right.write(center);
     delay(250);
   } else {
     //resume driving straight
-    leftServo.write(0);
-    rightServo.write(180);
+    left.write(0);
+    right.write(180);
   }
 
 }
--- examples/3-Projects/LineFollowingBot/LineFollowingBot.ino
+++ examples/3-Projects/LineFollowingBot/LineFollowingBot.ino
@@ -8,72 +8,75 @@
 / Questions? Concerns? Bugs? email code@mymindsi.com
 /
 / This example expects 2 servos in pins 4 and 5
-/ and three QTI sensors in pins A0, A1, and A2
+/ three QTI sensors in pins A0, A1, and A2
+/ and a forward facing ping sensor on pin 9
 /***************************************************/
 
-Servo leftServo, rightServo;
-bool left, middle, right;
+Servo left, right;
 
 float Lout, Rout;
 float Lset, Rset;
 
-unsigned long time;
-double dT;
-double adj;
-
-const int center =  90;
+const double rxtime    = 0.25;
+const int neutral      = 90;
+const int leftCenter   = 0;
+const int rightCenter  = 0;
+const int outsideSpeed = 45;
+const int insideSpeed  = 12;
+const int forwardSpeed = 35;
+const int hazardDist   = 750;
 
 void setup() {
-  leftServo.attach(5);
-  rightServo.attach(4);
+  Serial.begin(9600);
 
-  leftServo.write(center);
-  rightServo.write(center);
+  left.attach(5);
+  right.attach(4);
 
-  Rout = center;
-  Lout = center;
+  left.write(neutral);
+  right.write(neutral);
+
+  Rout = neutral;
+  Lout = neutral;
 }
 
 void loop() {
-  left = QTI(A0) > 120;
-  middle = QTI(A1) > 120;
-  right = QTI(A2) > 120;
+  bool left   = QTI(A0) > 120;
+  bool middle = QTI(A1) > 120;
+  bool right  = QTI(A2) > 120;
 
   if (left) {
-    Lset = center + 6;
-    Rset = center + 45;
+    Lset = neutral + insideSpeed;
+    Rset = neutral + outsideSpeed;
   }
   else if (right) {
-    Lset = center - 45;
-    Rset = center - 6;
+    Lset = neutral - outsideSpeed;
+    Rset = neutral - insideSpeed;
   }
   else if (middle) {
-    Lset = center - 45;
-    Rset = center + 45;
+    Lset = neutral - forwardSpeed;
+    Rset = neutral + forwardSpeed;
   }
 
-  if (getPing(9) < 750) {
-    Rset = center;
-    Lset = center;
+  if (getPing(9) < hazardDist) {
+    Rset = neutral;
+    Lset = neutral;
   }
 
   output();
 }
 
+//all this is because the QTI Blocks for a variable amount of time
 void output() {
-  //all this is because the QTI Blocks for a variable amount of time
-#define rxtime .25
+  static uint32_t time = millis();
 
-  dT = ( double(millis() - time) / 100) ;
+  double dT = ( double(millis() - time) / 100) ;
   time = millis();
 
-  adj = pow(rxtime, dT);
+  double adj = pow(rxtime, dT);
 
   Rout = ( Rout * adj ) + ( Rset * (1 - adj) );
   Lout = ( Lout * adj ) + ( Lset * (1 - adj) );
 
-  leftServo.write(Lout);
-  rightServo.write(Rout);
+  left.write(Lout + leftCenter);
+  right.write(Rout+ rightCenter);
 }
-
-
